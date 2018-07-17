0df20d0452761695934a84c29714c7ff43070299
Ronshmul <ronush.92@gmail.com>
2018-07-18 00:24:42 +0300

    Register validation

--- client/components/Home/regCtrl.js
+++ client/components/Home/regCtrl.js
@@ -2,10 +2,66 @@ angular.module('citiesApp')
     .controller('regCtrl', ['$http','$scope', '$rootScope', 'setToken', '$location',
     function($http, $scope, $rootScope, setToken, $location) {
         let serverUrl = 'http://localhost:3000/';
-        $scope.submit = function(isValid) {
-            $("#goUpArrow").hide();
+        self = this;
+        
+        $scope.numOfCategories=0;
+        /**
+         * All the preperation
+         */
 
+        //gets the categories from the server
+        getCategories=function(){  //Todo : put this function in service for reuse
+            $http.get(serverUrl +"poi/getCategories")
+            .then(function(response){
+                $scope.categories = response.data.Categories;
+            },function(response){
+                $scope.categories = "Something went wrong";
+            })
+        }
+        getCategories();
+
+        //checks how many categories were chosen
+        $scope.sumCategories=function(checked){
+            if(checked){
+                $scope.numOfCategories++;        
+            }            
+            else{
+                $scope.numOfCategories--; 
+            }
+        }
+
+        //gets the countries from an xml file
+        self.getCountries = function(){
+            var req = new XMLHttpRequest();
+            req.onreadystatechange = function(){
+                var result =[]
+                if(this.readyState == 4 && this.status == 200){
+                    var xml = this.responseXML;
+                    var xmlcountries = xml.getElementsByTagName("Country");
+                    for(var i=0; i<xmlcountries.length; i++ ){
+                        var country = {
+                            "id": xmlcountries[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue.toString(),
+                            "Name": xmlcountries[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue.toString()
+                        }
+                        result.push(country);
+                    }
+                    $scope.countries = result;
+                }
+            }           
+            req.open("GET","../../countries/countries.xml", true);
+            req.send();
+       }
+       
+       self.getCountries();
+
+        /**
+         * The submit
+         */
+
+        $scope.submit = function() {
+            $("#goUpArrow").hide();
             isValid = true;
+            
             if(isValid) {
                 $http.post(serverUrl +"users/register", JSON.stringify($rootScope.User))
                 .then(function(response){
